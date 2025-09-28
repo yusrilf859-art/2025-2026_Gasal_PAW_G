865259f18c0b52f5d8e36ba9302aa023b26ccc45
Farihinsyah <eric46284@gmail.com>
2025-09-28 19:47:16 +0700

    Add files via upload

---
+++ Modul-1/24-031_Ahmad_Syahrul_Farihin/soal3.php
@@ -0,0 +1,24 @@
+<?php
+echo "Hello world" . "<br>";
+
+// ini non-embedded script
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Hello world";
+    
+    // ini embedded-script
+    ?>
+</body>
+</html>
