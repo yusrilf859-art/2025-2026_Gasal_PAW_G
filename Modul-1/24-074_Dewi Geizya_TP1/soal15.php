8c2001d5d4f4dd5b9d18543f09f12341a1aeb5c6
Dewi Geizya <dewigeizya@gmail.com>
2025-09-28 16:03:19 +0700

    Add files via upload

---
+++ Modul-1/24-074_Dewi Geizya_TP1/soal15.php
@@ -0,0 +1,9 @@
+<?php
+function familyName($fname, $year) {
+  echo "$fname $year <br>";
+}
+
+familyName("Hege born in","1975");
+familyName("Stale born in","1978");
+familyName("Kai Jim born in","1983");
+?>
---
+++ Modul-1/24-074_Dewi Geizya_TP1/soal3.php
@@ -0,0 +1,18 @@
+<?php
+// ini non-embeded script
+echo "Hello World";
+echo "<hr>"
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Document</title>
+</head>
+<body>
+ <?php
+ // echo ini embedded-script
+ echo "Hello World"; ?>
+</body>
+</html>
