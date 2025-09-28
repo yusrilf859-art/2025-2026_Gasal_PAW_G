61efa91c210fff94c8c9ce7cc4b9c7916c8c385e
24-154_Ainur Suharyanto <ainursuharyanto8@gmail.com>
2025-09-28 23:28:17 +0700

    Add files via upload

---
+++ Modul-1/24-154_Ainur_Suharyanto/p15.php
@@ -0,0 +1,11 @@
+<?php
+
+function familyName2($fname, $year) {
+    echo "$fname Born in $year <br>";
+}
+
+familyName2("Hege", "1975");
+familyName2("Stale", "1978");   
+familyName2("Kai Jim", "1983");
+
+?>
