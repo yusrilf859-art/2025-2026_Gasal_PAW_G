d3a04ca78ca11075d19dc32031a042a9438939ba
biyann12 <kadada735@gmail.com>
2025-09-28 22:13:39 +0700

    Add files via upload

---
+++ Modul-1/24-231_Moch_Zibyan_Kadada/soal 15.php
@@ -0,0 +1,9 @@
+<?php  
+function familyName($fname,$year){ 
+    return "$fname born in $year"; 
+} 
+echo familyName("Hage",1975)."<br>"; 
+echo familyName("Stale",1978)."<br>"; 
+echo familyName("Kai Jim",1983);
+?> 
+ 
---
+++ Modul-1/24-231_Moch_Zibyan_Kadada/soal 2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Soal 2</title>
+</head>
+<body>
+    <h1><?php
+    //ini embedded-script
+    echo "Hello World";
+    ?></h1>
+</body>
+</html>
