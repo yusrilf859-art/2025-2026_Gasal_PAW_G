4d810b230e1c4399c057c9115eeacda7cc445a73
afrizarasya <afrizahabibi13@gmail.com>
2025-09-28 22:40:25 +0700

    Add files via upload

---
+++ Modul-1/24-199_Muhammad_Afriza_Rasya_Habibi/Soal3.php
@@ -0,0 +1,17 @@
+<?php
+// ini non-embeded script
+echo "Hello World";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Soal 2</title>
+</head>
+<body>
+    <?php
+    //ini embedded-script
+    echo "Hello World";
+    ?>
+</body>
+</html>
