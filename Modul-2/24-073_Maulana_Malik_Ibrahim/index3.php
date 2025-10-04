f82277e8ec3e8f5dd7ef2bae6ce7a14d4c3da39c
24-073_Maulana_Malik_Ibrahim <160454081+ibrahimzyy@users.noreply.github.com>
2025-10-04 21:45:11 +0700

    Add files via upload

---
+++ Modul-2/24-073_Maulana_Malik_Ibrahim/index3.php
@@ -0,0 +1,13 @@
+<center>
+    <?php
+    $angka = 0;
+
+    // Perulangan Do While
+    do {
+        echo "$angka<br>";
+        $angka += 4; // naik 4 setiap perulangan
+    } while ($angka <= 20);
+         echo "<td> </td>";
+         echo "<tr> </tr>";
+    ?>
+</center>
