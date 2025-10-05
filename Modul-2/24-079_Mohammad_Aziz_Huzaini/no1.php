2c4c134875162783856f1570c2b97904a7c1499b
MohammadAziz <106005582+4212hz@users.noreply.github.com>
2025-10-05 15:19:59 +0700

    Add files via upload

---
+++ Modul-2/24-079_Mohammad_Aziz_Huzaini/no1.php
@@ -0,0 +1,17 @@
+<?php
+$matkul = ["PTI", "ALPRO", "DPW", "STRUKDAT", "JARKOM", "PAW", "PSBF", "RPL"];
+$praktikum = ["JARKOM", "PAW"];
+$jumlah_matkul = count($matkul);
+
+for ($i = 0; $i < $jumlah_matkul; $i++) {
+	if (in_array($matkul[$i], $praktikum)) {
+		echo "Saya sedang mengambil matkul {$matkul[$i]} termasuk praktikumnya<br>";
+	}
+	elseif ($i === 6 || $i === 7) {
+		echo "Saya belum mengambil matkul {$matkul[$i]}<br>";
+	}
+	else {
+		echo "Saya sudah mengambil matkul {$matkul[$i]} semester lalu<br>";
+	}
+}
+?>
