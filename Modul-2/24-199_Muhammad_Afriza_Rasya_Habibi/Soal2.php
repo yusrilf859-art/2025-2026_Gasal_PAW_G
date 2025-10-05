378ed98a3413cdfda59b8ae4b145f66258bc6b6c
afrizarasya <afrizahabibi13@gmail.com>
2025-10-05 19:29:17 +0700

    Add files via upload

---
+++ Modul-2/24-199_Muhammad_Afriza_Rasya_Habibi/Soal2.php
@@ -0,0 +1,29 @@
+<?php
+$matkul = ["PTI", "ALPRO", "DPW", "STRUKDAT", "JARKOM", "PAW", "PSBF", "RPL"];
+
+foreach ($matkul as $mk) {
+    switch ($mk) {
+        case "PTI":
+            echo "Saya suka $mk<br>";
+            break;
+        case "ALPRO":
+            echo "Saya suka $mk<br>";
+            break;
+        case "DPW":
+            echo "Saya suka $mk<br>";
+            break;
+        case "STRUKDAT":
+            echo "Saya suka $mk<br>";
+            break;
+        case "JARKOM":
+            echo "Saya suka $mk<br>";
+            break;
+        case "PAW":
+            echo "Saya suka $mk<br>";
+            break;
+        default:
+            echo "Saya tidak mengambil matkul $mk<br>";
+            break;
+    }
+}
+?>
