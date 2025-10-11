a15c9b70825a0eae3a8b27e2e0f90e1d9be9219a
yusrilf859-art <yusrilf859@gmail.com>
2025-10-11 18:15:29 +0700

    Add files via upload

---
+++ Modul-3/24_229_Muhammad_Yusril_Firmansyah/3.4.2.php
@@ -0,0 +1,10 @@
+<?php
+$weight = array(
+    "Andy" => "70",
+    "Barry" => "65",
+    "Charlie" => "68"
+);
+foreach($weight as $x => $x_value) {
+    echo "Key=" . $x . ", Value=" . $x_value . "<br>";
+}
+?>
