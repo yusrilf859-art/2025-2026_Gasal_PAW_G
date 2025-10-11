35736d8bc7fbd81a6659885b9dca2d5a8c5bec0d
yusrilf859-art <yusrilf859@gmail.com>
2025-10-11 18:14:03 +0700

    Add files via upload

---
+++ Modul-3/24_229_Muhammad_Yusril_Firmansyah/3.3.3.php
@@ -0,0 +1,9 @@
+<?php
+$weight = array(
+    "Andy" => "70",
+    "Barry" => "65",
+    "Charlie" => "68"
+);
+$values = array_values($weight);
+echo "Data ke-2 dari array \$weight adalah: " . $values[1] . " kg";
+?>
