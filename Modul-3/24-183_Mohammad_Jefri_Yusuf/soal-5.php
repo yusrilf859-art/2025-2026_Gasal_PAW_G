2a75aaf516e1abe0f9821071ad409e2ce37b9735
Mohammad Jefri Yusuf <mohammadjefriyusuf@gmail.com>
2025-10-12 09:29:29 +0700

    Edit soal-5.php

--- Modul-3/24-183_Mohammad_Jefri_Yusuf/soal-5.php
+++ Modul-3/24-183_Mohammad_Jefri_Yusuf/soal-5.php
@@ -1,5 +1,5 @@
 <?php 
-$student = array(
+$students = array(
     array("Alex", "220401", "0812345678"),
     array("Bianca", "220402", "0812345687"),
     array("Candice", "220403", "0812345665")
@@ -8,7 +8,7 @@ for($row = 0; $row < 3; $row++) {
     echo "<p><b>Row number $row</p></b>";
     echo "<ul>";
     for ($col = 0; $col < 3; $col++){
-        echo "<li>".$student[$row][$col]."</li>";
+        echo "<li>".$students[$row][$col]."</li>";
 
     }
     echo "</ul>";
@@ -16,26 +16,27 @@ for($row = 0; $row < 3; $row++) {
 echo "<hr>";
 
 // 3.5.1
-$student[] = ["Jefri","220404","0812345666"];
-$student[] = ["Lewis","220405","0812345667"];
-$student[] = ["Fuzuka","220406","0812345668"];
-$student[] = ["Jordan","220407","0812345669"];
-$student[] = ["Edward","220408","0812345670"];
+$students[] = ["Jefri","220404","0812345666"];
+$students[] = ["Lewis","220405","0812345667"];
+$students[] = ["Fuzuka","220406","0812345668"];
+$students[] = ["Jordan","220407","0812345669"];
+$students[] = ["Edward","220408","0812345670"];
 
-for($row = 0; $row < count($student); $row++) {
+for($row = 0; $row < count($students); $row++) {
     echo "<p><b>Row number $row</p></b>";
     echo "<ul>";
     for ($col = 0; $col < 3; $col++){
-        echo "<li>".$student[$row][$col]."</li>";
+        echo "<li>".$students[$row][$col]."</li>";
 
     }
     echo "</ul>";
 }
 echo "<hr>";
 
-// 3.5.2
+
 ?>
 
+<!-- 3.5.2 -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -50,10 +51,10 @@ echo "<hr>";
             <th>NIM</th>
             <th>Mobile</th>
         </tr>
-        <?php for($row = 0; $row < count($student); $row++): ?>
+        <?php for($row = 0; $row < count($students); $row++): ?>
             <tr>
-                <?php for($col = 0; $col < count($student[0]); $col++): ?>
-                    <td><?= $student[$row][$col]; ?></td>
+                <?php for($col = 0; $col < count($students[0]); $col++): ?>
+                    <td><?= $students[$row][$col]; ?></td>
                 <?php endfor; ?>
             </tr>
         <?php endfor; ?>
